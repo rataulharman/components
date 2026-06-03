51ea038a927a130b17c0d4ff7832eced28997aa0
Harman <harmandeepsingh7@student.mitt.ca>
2026-06-02 19:10:35 -0500

    Header setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Header from "./components/Header";
+
 
 function App() {
+
+  const navLinks = [
+    { index: 0, name: "Home", url: "#" },
+    { index: 1, name: "About", url: "#" },
+    { index: 2, name: "Gallery", url: "#" },
+    { index: 3, name: "Contact", url: "#" }
+  ];
+
   return (
-    <></>
+    <>
+      <Header navLinks={navLinks} />
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
-
-function Header() {
+function Header(props) {
   return (
-    <></>
+    <header>
+      <div className="container flex">
+        <h1>React</h1>
+
+        <nav>
+          {props.navLinks.map(link => (
+            <a key={link.index} href={link.url}>
+              {link.name}
+            </a>
+          ))}
+        </nav>
+
+      </div>
+    </header>
   );
 }
 
